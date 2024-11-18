6d7bbbd49474342736c68cc5b7c9436bb4207e0c
Michael Schuh <michael.schuh.34@gmail.com>
2024-11-18 23:14:08 +0100

    refactor word2vec, regenerate BoW and Word2Vec analysis

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -5,6 +5,14 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 import umap
 
+from gensim.models import Word2Vec
+from gensim.utils import simple_preprocess
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Embedding, GlobalAveragePooling1D
+import multiprocessing
+
 def pca_graph(X):
     pca = PCA()
     pca.fit_transform(X)
@@ -47,3 +55,70 @@ def umap_plt(embed_X, y):
 
 def umap_graph(embed_X, y):
     umap_plt(embed_X, y).show()
+
+def word2vec_embed(sentences):
+    w2v_size=300
+    w2v_window=5
+    w2v_min_count=1
+    w2v_epochs=100
+    maxlen = 24 # adapt to length of sentences
+    sentences = [simple_preprocess(text) for text in sentences]
+
+    w2v_model = Word2Vec(min_count=w2v_min_count, window=w2v_window,
+                                                vector_size=w2v_size,
+                                                seed=314,
+                                                # workers=1)
+                                               workers=multiprocessing.cpu_count())
+
+    w2v_model.build_vocab(sentences)
+    w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=w2v_epochs)
+
+    model_vectors = w2v_model.wv
+    w2v_words = model_vectors.index_to_key
+
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(sentences)
+    x_sentences = pad_sequences(tokenizer.texts_to_sequences(sentences),
+                                                         maxlen=maxlen,
+                                                         padding='post') 
+                                                       
+    num_words = len(tokenizer.word_index) + 1
+
+    # weights matrix
+    print("Create Embedding matrix ...")
+    w2v_size = 300
+    word_index = tokenizer.word_index
+    vocab_size = len(word_index) + 1
+    embedding_matrix = np.zeros((vocab_size, w2v_size))
+    i=0
+    j=0
+        
+    for word, idx in word_index.items():
+        i +=1
+        if word in w2v_words:
+            j +=1
+            embedding_vector = model_vectors[word]
+            if embedding_vector is not None:
+                embedding_matrix[idx] = model_vectors[word]
+                
+    word_rate = np.round(j/i,4)
+    print("Word embedding rate : ", word_rate)
+    print("Embedding matrix: %s" % str(embedding_matrix.shape))
+
+    # Model creation
+
+    input=Input(shape=(len(x_sentences),maxlen),dtype='float64')
+    word_input=Input(shape=(maxlen,),dtype='float64')  
+    word_embedding=Embedding(input_dim=vocab_size,
+                             output_dim=w2v_size,
+                             weights = [embedding_matrix],
+                             input_length=maxlen)(word_input)
+    word_vec=GlobalAveragePooling1D()(word_embedding)  
+    embed_model = Model([word_input],word_vec)
+    
+    embed_model.summary()
+
+    embeddings = embed_model.predict(x_sentences)
+    print(embeddings.shape)
+
+    return embeddings
